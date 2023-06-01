5345bd0bd86090bfa26775a369d94ac2c14a647b
Mateusz Gajewski <mateo31313@gmail.com>
2023-06-01 22:19:57 +0200

    tipical message360 this is my message360

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ new class main {
     public static void main(String[] args) {
         System.out.println(Hello world!);
         System.out.println(Hello world2!);
-
+        System.out.println(Hello world3!);
 
     }
 }
