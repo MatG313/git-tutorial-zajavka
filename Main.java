8bb185c2a862df8b9d4084adb1519f15fdfdd5d3
Mateusz Gajewski <mateo31313@gmail.com>
2023-06-01 22:23:09 +0200

    this is my typical message xfcghdrxgbxddgzxgzxf

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ new class main {
         System.out.println(Hello world2!);
         System.out.println(Hello world3!)
         System.out.println(Hello world4 !)
-
+        System.out.println(Hello world5!)
 
     }
 }
