1920bcece5d5787eb2277fb9b22ac553855b5c85
Mateusz Gajewski <mateo31313@gmail.com>
2023-06-01 22:20:58 +0200

    this is my typical message 420

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ new class main {
     public static void main(String[] args) {
         System.out.println(Hello world!);
         System.out.println(Hello world2!);
+        System.out.println(Hello world4!);
 
 
     }
